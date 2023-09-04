7210ab8c44e8a9b58e132260fd2c8899cd347306
kirilllesniak <cracycot@mail.ru>
2023-09-04 19:31:33 +0300

    Initial Commit

---
+++ learning c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learning c++
+//
+//  Created by Kirill Lesniak on 04.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
